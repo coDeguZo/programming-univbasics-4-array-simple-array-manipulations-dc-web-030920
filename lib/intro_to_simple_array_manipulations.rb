52e62a3976c85da85cfd1fa8d9df532a542e98cd
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-04 20:21:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -76,8 +76,8 @@ end
 
 def using_delete_at(array, integer)
   famous_robots = ["Johnny 5", "R2D2", "Robocop"]
-  delete_robot = famous_robots.delete_at(integer)
-  delete_robot
+  famous_robots.delete_at(integer)
+  famous_robots
 end
 
 

ed9431a2d6a4a0a3c01037161d4c8bd249733c78
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-04 19:56:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,7 +39,9 @@ end
 
 
 def using_concat(array1, array2)
-  
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    
   
 end
 

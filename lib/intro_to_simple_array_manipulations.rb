6c33317be744fd55f85359f5f2a34439f6a22106
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-04 19:43:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,7 +13,6 @@ end
 def using_pop(array)
 continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
 continents_pop = continents.pop
-p continents
 continents_pop
 end
 

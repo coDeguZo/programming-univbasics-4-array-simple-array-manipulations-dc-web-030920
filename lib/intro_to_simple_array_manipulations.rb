1b58393ec39aa20b04b601ea837a340577c841e4
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-04 20:01:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -48,6 +48,7 @@ end
 
 def using_insert(array, element)
   @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  @another_language = "Python"
   
 end
 
